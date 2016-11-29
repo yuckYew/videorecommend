2445e936933b09b4a81815385855a998e1fa64a7
Yusuke Chayama <yusukechayama@gmail.com>
2016-11-29 13:56:42 +0000

    Everything done till tutorial pt 4

--- mybideo/urls.py
+++ mybideo/urls.py
@@ -2,6 +2,10 @@ from django.conf.urls import url
 
 from . import views
 
+app_name = 'mybideo'
 urlpatterns = [
+    # ex: /mybideo/
     url(r'^$', views.index, name='index'),
+    # ex: /mybideo/5/
+    url(r'^(?P<movie_id>[0-9]+)/$', views.detail, name='detail'),
 ]
--- mybideo/views.py
+++ mybideo/views.py
@@ -1,5 +1,14 @@
-from django.shortcuts import render
+from django.http import HttpResponse
+from django.shortcuts import get_object_or_404, render
+
+from .models import Movie
 
 # Create your views here.
 def index(request):
-    return HttpResponse("Hello, world. You're at the mybideo index.")
+    movie_list = Movie.objects.all()
+    context = {'movie_list': movie_list}
+    return render(request, 'mybideo/index.html', context)
+
+def detail(request, movie_id):
+    movie = get_object_or_404(Movie, pk=movie_id)
+    return render(request, 'mybideo/detail.html', {'movie': movie})

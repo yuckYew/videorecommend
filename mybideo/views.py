d56e3a0f6c773bcf8b750ed11db6ee07a713e0f4
Yusuke Chayama(Chasen) <yusukechayama@gmail.com>
2016-12-05 14:22:12 +0900

    Make demo UI

--- mybideo/views.py
+++ mybideo/views.py
@@ -1,3 +1,6 @@
+import numpy as np
+import random
+
 from django.shortcuts import get_object_or_404, render
 from django.http import HttpResponse, HttpResponseRedirect
 from django.urls import reverse
@@ -6,13 +9,22 @@ from django.views import generic
 from .models import Movie, MovieInfo
 
 # Create your views here.
+"""
+def index(request):
+    movie_all = np.array(Movie.objects.all())
+    choice_index = random.sample(range(1, len(movie_all)), 3)
+    movie_list = list(movie_list[choice_index])
+    context = {'movie_list', movie_list}
+    return render(request, 'mybideo/index.html', context)
+"""
 class IndexView(generic.ListView):
     template_name = 'mybideo/index.html'
     context_object_name = 'movie_list'
 
     def get_queryset(self):
         """Return list of movies"""
-        return Movie.objects.all()
+        # return Movie.objects.all()[0:3]
+        return list(np.random.choice(Movie.objects.all(), size=3, replace=False))
 
 class DetailView(generic.DetailView):
     model = Movie
--- scripts/read_json.py
+++ scripts/read_json.py
@@ -2,7 +2,9 @@ import json
 from pprint import pprint as pp
 
 with open('movie_data.json', 'r') as rf:
-    movie_data = json.load(rf)
+    parsed_json = json.load(rf)
 
-print(type(movie_data))
-pp(movie_data)
+print(parsed_json[0]['title'])
+
+for movie in parsed_json:
+    parsed_json[0]['title'] = pp(movie['title'])
